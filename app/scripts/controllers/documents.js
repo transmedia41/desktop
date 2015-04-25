e45a96f85eec4811c8e197bd298996a061c5ca8d
umelec <info@fclerc.ch>
2015-04-25 13:31:13 +0200

    je hais grunt - documents files créé

--- app/scripts/controllers/documents.js
+++ app/scripts/controllers/documents.js
@@ -7,8 +7,95 @@
  * # MainCtrl
  * Controller of the deskappApp
  */
-angular.module('deskappApp')
+ angular.module('deskappApp')
 
-  .controller('DocumentsCtrl', function ($scope) {
-    // ...
-  })
+ .controller('DocumentsCtrl', function ($scope) {
+
+
+  var i = 1
+  this.click = function(i){
+    var r = i
+    console.log('click', r)
+  }
+
+
+  $scope.showDocumentContent = function(){
+    console.log('click')
+    $('.imGD').show()
+
+  }
+
+
+  $scope.documentsList = [
+  {
+    "order":1,
+    "date":"",
+    "description":"Naissance de Paolo Salvatore",
+    "xp":"",
+    "documents":[
+    {
+      title: "Photo de naissance",
+      thumbnail: "img/imgPieceAConviction/imgPAC_thumb.jpg",
+      versionUrl: "",
+      src: "http://...",
+      type: "photo",
+      templateHtml:"http://",   
+      xp: "15"
+    },
+    {
+      title: "Acte de naissance",
+      thumbnail: "img/imgPieceAConviction/imgPAC_thumb.jpg",
+      versionUrl: "http://...",
+      src: "",
+      type: "image",
+      templateHtml:"http://...",    
+      xp: "16"
+    },
+       {
+      title: "Acte de naissance",
+      thumbnail: "img/imgPieceAConviction/imgPAC_thumb.jpg",
+      versionUrl: "http://...",
+      src: "",
+      type: "image",
+      templateHtml:"http://...",    
+      xp: "16"
+    },
+     {
+      title: "Acte de naissance",
+      thumbnail: "img/imgPieceAConviction/imgPAC_thumb.jpg",
+      versionUrl: "http://...",
+      src: "",
+      type: "image",
+      templateHtml:"http://...",    
+      xp: "16"
+    }
+    ]
+  },
+  {
+    "order":2,
+    "date":"",
+    "description":"Emprisonnement de Daniele Salvatore",
+    "xp":"",
+    "documents": [
+    {
+      title: "Vidéosurveillance transaction",
+      thumbnail: "img/imgPieceAConviction/imgPAC_thumb.jpg",
+      versionUrl: "",
+      src: "http://...",
+      type: "video",
+      templateHtml:"http://",   
+      xp: "15"
+    }
+    ]
+  }
+
+
+  ]// \.documentsList
+
+
+
+})
+
+
+
+
