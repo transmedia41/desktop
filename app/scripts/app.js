976ac500a51d2394996dea3dafeb3d67e2dcd0f2
GuggerJoel <joel@thimoo.ch>
2015-04-28 11:14:31 +0200

    add GameCoreCtrl

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -205,4 +205,60 @@ angular
   })
 
 
+ .service('SectorService', function(localStorageService, SocketService){
+
+    var sectors = []
+    
+    function getListSectors(callback) {
+      SocketService.getSocket()
+        .emit('get sectors')
+        .on('sectors responce', function(data){
+          localStorageService.set('sectors', data)
+          localStorageService.set('last update sectors', Date.now())
+          //console.log('get sectors')
+          sectors = data
+          callback(sectors)
+        })
+    }
+
+    var service = {
+      getSectors: function(callback) {
+        if(localStorageService.isSupported){
+          if(!localStorageService.get('sectors')){
+            getListSectors(callback)
+          } else {
+            var lastDisconnect
+            (!localStorageService.get('last disconnect')) ? lastDisconnect = 0 : lastDisconnect = localStorageService.get('last disconnect')
+            if(lastDisconnect > localStorageService.get('last update sectors')) {
+              getListSectors(callback)
+            } else {
+              sectors = localStorageService.get('sectors')
+              callback(sectors)
+            }
+          }
+        } else {
+          $rootScope.$emit('localstorage not supported')
+        }
+      },
+      onUpdate: function(callback) {
+        // use socket to track update and execute callback...
+        // update sectors and save into localstorage
+        socket.on('sectors update', function(data) {
+          callback(data)
+        })
+      }
+    }
+    return service
+
+  })
+
+ .controller('GameCoreCtrl', function ($scope) {
+    
+    // ...
+    
+  })
+
+
+
+
 
