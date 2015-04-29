7498799652603733fb7726173fb87263b138a1a1
GuggerJoel <joel@thimoo.ch>
2015-04-29 04:12:26 +0200

    add url prod

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,6 +29,7 @@ angular
    */
   .constant('Config', {
     API_URL: 'http://localhost:3000/',
+    //API_URL: 'http://hydromerta.comem.ch:9001/',
     mapboxMapId:  'hydromerta.lpkj6fe5',
     mapboxAccessToken: 'pk.eyJ1IjoiaHlkcm9tZXJ0YSIsImEiOiJZTUlDdVA0In0.Z7qJF3weLg5WuPpzt6fMdA'
   })
